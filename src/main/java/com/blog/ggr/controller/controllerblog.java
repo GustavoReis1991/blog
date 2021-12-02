11aa5717e45522fa090ad3c5d63cbc4f2da48875
GustavoReis1991 <gustavo.gomes.reis@hotmail.com>
2021-12-02 15:52:20 -0300

    blog

---
+++ src/main/java/com/blog/ggr/controller/controllerblog.java
@@ -0,0 +1,68 @@
+package com.blog.ggr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.ggr.model.modelblog;
+import com.blog.ggr.repositorio.repositorioblog;
+
+@RestController
+@RequestMapping("/blog.ggr")
+@CrossOrigin("*")
+
+public class controllerblog {
+	
+	@Autowired
+	private repositorioblog rg;
+	
+	@GetMapping
+	public ResponseEntity<List<modelblog>> getAll(){
+		return ResponseEntity.ok(rg.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<modelblog> GetById(@PathVariable long id){
+		return rg.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<modelblog>> GetByTitulo(@PathVariable String titulo)
+	{
+		return ResponseEntity.ok (rg.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<modelblog> post (@RequestBody modelblog postagem)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(rg.save(postagem));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<modelblog> put (@RequestBody modelblog postagem)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(rg.save(postagem));
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable long id)
+	{
+		rg.deleteById(id);
+	}
+
+}
---
+++ src/main/java/com/blog/ggr/repositorio/repositorioblog.java
@@ -0,0 +1,18 @@
+package com.blog.ggr.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.blog.ggr.model.modelblog;
+@Repository
+public interface repositorioblog extends JpaRepository<modelblog, Long>{
+
+	List<modelblog> findAllByTituloContainingIgnoreCase(String titulo);
+
+	
+			
+	
+	
+
+}
